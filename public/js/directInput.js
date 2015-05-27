f01578d823dd37122eb680103d2f3218179a36e9
Lars Laptop <larskarbo@gmail.com>
2015-05-27 22:31:42 +0200

    Initialized the directInput.js module

---
+++ public/js/directInput.js
@@ -0,0 +1,17 @@
+/*
+
+This module adds support for modifying a schedule
+directly on the canvas with mouse or touch
+
+*/
+
+window.directInput = (function(){
+	//public:
+
+
+	//private:
+	return{
+
+	}
+	
+}());
