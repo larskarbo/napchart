7cc607e4d406c8b9b4e7af679086db7a9b5f121d
Lars Laptop <larskarbo@gmail.com>
2015-05-28 17:06:22 +0200

    Added a requestanimframe helpers function with polyfill

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -1,5 +1,27 @@
 window.helpers = {};
 
+//Request animation polyfill - http://www.paulirish.com/2011/requestanimationframe-for-smart-animating/
+helpers.requestAnimFrame = (function(){
+    return window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function(callback) {
+            return window.setTimeout(callback, 1000 / 60);
+        };
+}());
+helpers.cancelAnimFrame = (function(){
+    return window.cancelAnimationFrame ||
+        window.webkitCancelAnimationFrame ||
+        window.mozCancelAnimationFrame ||
+        window.oCancelAnimationFrame ||
+        window.msCancelAnimationFrame ||
+        function(callback) {
+            return window.clearTimeout(callback, 1000 / 60);
+        };
+}());
+
 helpers.shortestWay = function (s) {
     console.log(s);
     if (s < -720)
