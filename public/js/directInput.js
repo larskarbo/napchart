3323304c96608b43fd8b56e9dc8caeec967390b0
Lars Laptop <larskarbo@gmail.com>
2015-06-01 10:57:58 +0200

    fix dragging whole bars, half finished hovering handles

--- public/js/directInput.js
+++ public/js/directInput.js
@@ -14,7 +14,7 @@ window.directInput = (function(){
 	activeElements = [];
 
 	function getRelativePosition(e){
-		var canvas = e.currentTarget || e.srcElement,
+		var canvas = e.target || e.srcElement,
 		boundingRect = canvas.getBoundingClientRect();
 
 		if (e.touches){
@@ -33,12 +33,49 @@ window.directInput = (function(){
 		};
 	}
 
+	function getCoordinates(e,canvas){
+		//similar to getrelativeposition but here origo is (0,0)
+		boundingRect = canvas.getBoundingClientRect();
+
+		if (e.touches){
+			mouseX = e.touches[0].clientX - boundingRect.left;
+			mouseY = e.touches[0].clientY - boundingRect.top;
+		}
+
+		else{
+			mouseX = e.clientX - boundingRect.left;
+			mouseY = e.clientY - boundingRect.top;
+		}
+
+		return {
+			x : mouseX-canvas.width/2,
+			y : mouseY-canvas.height/2
+		};
+	}
+
 	function hover(e){
-		var relativePosition = getRelativePosition(e);
+		var relativePosition = getRelativePosition(e),
+		data = napchartCore.getSchedule,
+		barConfig = draw.getBarConfig,
+		points = [], point, value;
 
 		mouseX = relativePosition.x;
 		mouseY = relativePosition.y;
+
+		//draws a new frame and checks for hit detection on bars
 		helpers.requestAnimFrame.call(window,draw.drawUpdate);
+
+		//hit detection of handles (will overwrite current mouseHover object
+		//from draw if hovering a handle):
+		for(name in data){
+			for(i = 0; i < data[name].length; i++){
+				for(s = 0; s < 2; s++){
+					value = data[name][i][['start','end'][s]];
+					point = helpers.minutesToXY(value , barConfig[name].outerRadius*draw.ratio);
+					//distance
+				}
+			}
+		}
 	}
 
 	function mouseLeave(e){
@@ -50,26 +87,27 @@ window.directInput = (function(){
 		e.preventDefault();
 
 		//return of no hit
-		if(typeof mouseHover == {}){
+		if(typeof mouseHover.name == 'undefined'||typeof mouseHover.count == 'undefined'){
 			return
 		}
 
-		var relativePosition = getRelativePosition(e),
-		minutes = helpers.XYtoMinutes(relativePosition.x,relativePosition.y)
-		name,
-		count,
-		positionInElement,
-		element;
-
-
-		name = mouseHover.name;
-		count = mouseHover.count;
-		element = napchartCore.returnElement(name,count);
-
-		console.log('hit',name,count);
-
+		var canvas = e.target || e.srcElement,
+		coordinates = getCoordinates(e,canvas),
+		minutes = helpers.XYtoMinutes(coordinates.x,coordinates.y),
+		name = mouseHover.name,
+		count = mouseHover.count,
+		element = napchartCore.returnElement(name,count),
 		positionInElement=helpers.calc(minutes,-element.start);
-		activeElements.push({name:name,count:count,position:positionInElement,what:'dragWhole'});
+		
+
+
+		activeElements.push({
+			name:name,
+			count:count,
+			positionInElement:positionInElement,
+			what:'dragWhole',
+			canvas:canvas
+		});
 
 
 		document.addEventListener('mousemove',drag);
@@ -81,20 +119,26 @@ window.directInput = (function(){
 	}
 
 	function drag(e){
+
 		var dragElement = activeElements[0],
-		relativePosition = getRelativePosition(e),
-		minutes = helpers.XYtoMinutes(relativePosition.x,relativePosition.y),
+		coordinates = getCoordinates(e,dragElement.canvas),
+		minutes = helpers.XYtoMinutes(coordinates.x,coordinates.y),
 		name = dragElement.name,
 		count = dragElement.count,
+		element = napchartCore.returnElement(name,count),
 		positionInElement = dragElement.positionInElement,
-		start = calc(minutes,-positionInElement);
+		duration = helpers.range(element.start,element.end),
+		start = helpers.calc(minutes,-positionInElement),
+		end = helpers.calc(start,duration);
+		newValues = {};
+
 
 		//newValues is an object that will replace the existing one with new values
-		newValues = {}
-
 		if(dragElement.what=='dragWhole'){
-			napchartCore.modifyElement(name,count,newValues)
+			newValues = {start:start,end:end}
 		}
+		
+		napchartCore.modifyElement(name,count,newValues);
 
 
 	}
@@ -126,7 +170,6 @@ window.directInput = (function(){
 		},
 
 		setHoverElements:function(hover){
-		console.log(mouseHover.name,mouseHover.count);
 			mouseHover = hover;
 		},
 
