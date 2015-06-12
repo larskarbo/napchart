615ec6c28155a1a74463b6a50e89ee7c1d847cbc
Lars Laptop <larskarbo@gmail.com>
2015-06-12 12:30:28 +0200

    added uberman sampleschedule

--- public/js/sample_schedules.js
+++ public/js/sample_schedules.js
@@ -63,6 +63,7 @@ window.sampleSchedule = (function () {
 		monophasic:	{core:[{start:120, end:330 }, {start:360, end:450 } ], nap:[{start:960, end:980 } ] , busy:[{start:600, end:844 } ]},
 		siesta:	{core:[{start:320, end:430 }, {start:360, end:450 } ] , nap:[{start:960, end:980 } ] , busy:[{start:600, end:1044 } ]},
 		segmented: {busy:[{start:540,end:1020},{start:1080,end:1140},{start:1200,end:1260},{start:1320,end:1380}],core:[{start:1410,end:1530},{start:1620,end:1740},{start:1830,end:1950},{start:2040,end:2160}],nap:[{start:960,end:980},{start:1040,end:1060}]}
+	,uberman: {"core":[],"nap":[{"start":242,"end":262},{"start":1197,"end":1217},{"start":959,"end":979},{"start":479,"end":499},{"start":1439,"end":19},{"start":721,"end":741}],"busy":[{"start":503,"end":904}]}
 	}
 	
 
