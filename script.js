96592cfcacdf0baba2a9aa588d4bcb5f5d431b30
Lars Laptop <larskarbo@gmail.com>
2015-06-17 00:56:18 +0200

    fixing ejs logic

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var bodyParser=require('body-parser');
@@ -5,68 +6,75 @@ var redis = require('redis');
 var client = redis.createClient(); //creates a new client
 
 
-	client.on('connect', function() {
-		console.log('connected');
-	});
+client.on('connect', function() {
+	console.log('connected');
+});
 
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 
 app.set('view engine', 'ejs');
 
 app.get('/get/:chartid', function(req, res) {
+
 	var chartid=req.params.chartid;
 	client.get('chart:'+chartid, function(err, reply) {
-	res.writeHead(200, {"Content-Type": "application/json"});
-	res.end(reply);
+		res.writeHead(200, {"Content-Type": "application/json"});
+		res.end(reply);
 	})
+
 });
 
 app.post('/post', function(req, res) {
+
 	function idgen(){
 		alphabet = "abcdefghijklmnopqrstuwxyz0123456789";
 		id='';
 		for( var i=0; i < 5; i++ )
-        id += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
+			id += alphabet.charAt(Math.floor(Math.random() * alphabet.length));
 		return id;
 	}
-	console.log(req.body.data);
+
 	chartid=idgen();
-	console.log(chartid);
+
 	client.set('chart:'+chartid,req.body.data,function(err,reply){
 		res.writeHead(200);
 		res.end(chartid);
 	})
-	
 });
 
-app.get('*', function (req, res) {
-	res.render('pages/index',{chartid:null});
-});
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+	res.render('pages/index',{page:'about'});
+});
+
+app.get('/:chartid', function (req, res) {
+	var chartid = req.params.chartid;
+
+	res.render('pages/index',{page:'chart',chartid:req.params.chartid});
 });
 
 app.get('/', function (req, res) {
-	res.send('You are now on the index page!')
+	res.render('pages/index',{page:'chart',chartid:null});
 });
-app.get('/:chartid', function (req, res) {
-	console.log(req.params.chartid);
-  //res.render('pages/index',{chartid:req.params.chartid});
-	res.send('You are now on the index page!')
+
+app.get('*', function (req, res) {
+	res.send('404',404);
 });
+
+
+
 
 
 
 var server = app.listen(3000, function () {
 
-  var host = server.address().address;
-  var port = server.address().port;
+	var host = server.address().address;
+	var port = server.address().port;
 	console.log(server.address());
-  console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Example app listening at http://%s:%s', host, port);
 
 });
