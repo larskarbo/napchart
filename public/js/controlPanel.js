8861e08baa651e7fe5f20dac0e896acf92e8e0a1
Lars Laptop <larskarbo@gmail.com>
2015-06-17 00:19:20 +0200

    pruned modules

--- public/js/controlPanel.js
+++ public/js/controlPanel.js
@@ -13,4 +13,5 @@ window.controlPanel = (function(){
 	return {
 
 	}
+	
 }());
---
+++ public/js/server.js
@@ -0,0 +1,16 @@
+/**
+
+This module handles server interactions to save and load schedules with ajax
+
+**/
+
+window.server=(function(){
+	//private
+
+
+	//public:
+	return{
+
+	}
+
+}())
