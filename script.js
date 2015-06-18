3a3fa254bb214d801b87ec94a236623718aa1bd1
Lars Laptop <larskarbo@gmail.com>
2015-06-18 12:35:54 +0200

    debug heroku

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ if (process.env.REDISTOGO_URL) {
 	var redis = require("redis").createClient(rtg.port, rtg.hostname);
 
 	redis.auth(rtg.auth.split(":")[1]);
-	
+
 } else {
-    var redis = require("redis").createClient();//creates a new client
+    //var redis = require("redis").createClient();//creates a new client
 }
 
 
