decab8495d012c542944f7c9ad23231b2ef8b6ac
Lars Laptop <larskarbo@gmail.com>
2015-06-18 12:33:22 +0200

    fix redis to go for heroku

--- script.js
+++ script.js
@@ -2,11 +2,20 @@
 var express = require('express');
 var app = express();
 var bodyParser=require('body-parser');
-var redis = require('redis');
-var redisClient = redis.createClient(); //creates a new client
+
+if (process.env.REDISTOGO_URL) {
+	// inside if statement
+	var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+	var redis = require("redis").createClient(rtg.port, rtg.hostname);
+
+	redis.auth(rtg.auth.split(":")[1]);
+	
+} else {
+    var redis = require("redis").createClient();//creates a new client
+}
 
 
-redisClient.on('connect', function() {
+redis.on('connect', function() {
 	console.log('connected');
 });
 
@@ -22,7 +31,7 @@ app.set('view engine', 'ejs');
 app.get('/get/:chartid', function(req, res) {
 
 	var chartid=req.params.chartid;
-	redisClient.get('chart:'+chartid, function(err, reply) {
+	redis.get('chart:'+chartid, function(err, reply) {
 		res.writeHead(200, {"Content-Type": "application/json"});
 		res.end(reply);
 	})
@@ -41,7 +50,7 @@ app.post('/post', function(req, res) {
 
 	chartid=idgen();
 
-	redisClient.set('chart:'+chartid,req.body.data,function(err,reply){
+	redis.set('chart:'+chartid,req.body.data,function(err,reply){
 		res.writeHead(200);
 		res.end(chartid);
 	})
@@ -54,7 +63,7 @@ app.get('/about', function(req, res) {
 app.get('/:chartid', function (req, res) {
 	var chartid = req.params.chartid;
 
-	redisClient.get('chart:'+chartid, function(err, reply) {
+	redis.get('chart:'+chartid, function(err, reply) {
 
 		res.render('pages/index',{chartid:chartid,chart:reply});
 	})
