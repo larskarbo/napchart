3bc1eea1b3a0527c14475fc47a29e27f9d79f6cf
Lars <larskarbo@hotmail.com>
2015-10-10 15:59:26 +0200

    change how check for fromServer object

--- public/js/application.js
+++ public/js/application.js
@@ -6,11 +6,10 @@ This module starts the app
 $(document).ready(function(){
 	var data = {}, chartid = false;
 
-	if(typeof fromServer != 'undefined'){
+	if(typeof fromServer.data != 'undefined' && typeof fromServer.chartid != 'undefined'){
 		data = fromServer.data;
 		chartid = fromServer.chartid;
 	}
-
 	napchartCore.initialize(data);
 
 	if(chartid){
