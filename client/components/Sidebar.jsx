5879d985bcbad5500e2d90e46f4e0db36f228d78
lars <larskarbo@hotmail.com>
2017-07-09 17:51:29 +0200

    sidebar

--- client/components/Sidebar.jsx
+++ client/components/Sidebar.jsx
@@ -20,7 +20,15 @@ export default class App extends React.Component {
       		<TypesContainer />
       	</Expandable>
 
+        <Expandable title="Polyphasic sleep" onToggle={this.onToggle.bind(null, 'poly')} active={this.isActive('poly')}>
+          Submit feedback here
+        </Expandable>
+
         <Expandable title="Feedback" onToggle={this.onToggle.bind(null, 'feedback')} active={this.isActive('feedback')}>
+          Submit feedback here
+        </Expandable>
+
+        <Expandable title="About" onToggle={this.onToggle.bind(null, 'about')} active={this.isActive('about')}>
           Submit feedback here
         </Expandable>
       </div>
